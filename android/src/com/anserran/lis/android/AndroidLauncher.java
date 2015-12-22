46ea2746cc3c17b0d43ef9f593a2ad7a83e98a86
anserran <angelserranolaguna@gmail.com>
2015-12-22 22:06:06 +0100

    First commit

---
+++ android/src/com/anserran/lis/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.anserran.lis.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.anserran.lis.LifeInSpace;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LifeInSpace(), config);
+	}
+}
