e57d0745f7dbc2bcd3241e730e839f145e7bae67
anserran <angelserranolaguna@gmail.com>
2015-12-23 21:17:54 +0100

    Add keyboard controller and first level

--- core/src/main/java/com/anserran/lis/LifeInSpace.java
+++ core/src/main/java/com/anserran/lis/LifeInSpace.java
@@ -1,40 +1,101 @@
 package com.anserran.lis;
 
-import com.anserran.lis.systems.AngularVelocitySystem;
+import com.anserran.lis.components.Tags;
+import com.anserran.lis.components.commands.Commands;
+import com.anserran.lis.input.KeyboardInputProcessor;
+import com.anserran.lis.loader.LevelLoader;
+import com.anserran.lis.loader.LevelLoader.EntityData;
+import com.anserran.lis.loader.LevelLoader.LevelData;
 import com.anserran.lis.systems.CollisionSystem;
-import com.anserran.lis.systems.DebugRenderSystem;
-import com.anserran.lis.systems.RenderSystem;
-import com.anserran.lis.systems.VelocitySystem;
+import com.anserran.lis.systems.Mappers;
+import com.anserran.lis.systems.animation.AngularVelocitySystem;
+import com.anserran.lis.systems.animation.CommandsSystem;
+import com.anserran.lis.systems.animation.InterpolationSystem;
+import com.anserran.lis.systems.animation.VelocitySystem;
+import com.anserran.lis.systems.render.CollidersRenderSystem;
+import com.anserran.lis.systems.render.RenderSystem;
+import com.badlogic.ashley.core.Component;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.core.PooledEngine;
+import com.badlogic.ashley.utils.ImmutableArray;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.utils.ObjectMap.Entry;
 
 public class LifeInSpace extends ApplicationAdapter {
 
     private PooledEngine engine;
     private RenderSystem renderSystem;
+    private LevelLoader loader;
+    private ImmutableArray<Entity> tagged;
+    private Data data;
 
-    private SpriteBatch batch;
+    public Data getData() {
+        return data;
+    }
 
     @Override
     public void create() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         engine = new PooledEngine();
-        engine.addSystem(renderSystem = new RenderSystem(batch = new SpriteBatch()));
+        data = new Data(engine);
+
         engine.addSystem(new VelocitySystem());
         engine.addSystem(new AngularVelocitySystem());
         engine.addSystem(new CollisionSystem());
-        engine.addSystem(new DebugRenderSystem(batch));
+        engine.addSystem(new InterpolationSystem());
+        engine.addSystem(new CommandsSystem());
+        engine.addSystem((CollidersRenderSystem) (renderSystem = new CollidersRenderSystem()));
+        this.loader = new LevelLoader(engine);
+
+        tagged = engine.getEntitiesFor(Family.all(Tags.class).get());
+
+        Gdx.input.setInputProcessor(new KeyboardInputProcessor(this));
+        loadLevel("1");
     }
 
     @Override
     public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         float delta = Math.min(Gdx.graphics.getDeltaTime(), 1.0f / 33f);
         engine.update(delta);
     }
 
     @Override
     public void resize(int width, int height) {
-        renderSystem.resize();
+        renderSystem.resize(width, height);
+    }
+
+    public void loadLevel(String levelId) {
+        engine.removeAllEntities();
+        LevelData levelData = loader.load(levelId);
+        renderSystem.setGridSize(levelData.width, levelData.height);
+        for (EntityData entityData : levelData.entities) {
+            Entity entity = engine.createEntity();
+            if (entityData.id != null) {
+                Tags tags = engine.createComponent(Tags.class);
+                tags.add(entityData.id);
+                entity.add(tags);
+            }
+            for (Component component : entityData.components) {
+                entity.add(component);
+            }
+            engine.addEntity(entity);
+        }
+    }
+
+    public void run() {
+        for (Entry<String, Commands> e : data.getCommandsMap().entries()) {
+            for (Entity entity : tagged) {
+                Tags tags = Mappers.tags.get(entity);
+                if (tags.contains(e.key, false)) {
+                    entity.add(e.value);
+                    break;
+                }
+            }
+        }
+        data.getCommandsMap().clear();
     }
 }
---
+++ core/src/main/java/com/anserran/lis/input/KeyboardInputProcessor.java
@@ -0,0 +1,70 @@
+package com.anserran.lis.input;
+
+import com.anserran.lis.Data.Command;
+import com.anserran.lis.LifeInSpace;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+
+public class KeyboardInputProcessor implements InputProcessor{
+
+    private LifeInSpace controller;
+
+    public KeyboardInputProcessor(LifeInSpace controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode){
+            case Keys.ENTER:
+                controller.run();
+                break;
+            case Keys.SPACE:
+                controller.getData().addCommand(Command.THRUST);
+                break;
+            case Keys.A:
+                controller.getData().addCommand(Command.ROTATE_CCW);
+                break;
+            case Keys.S:
+                controller.getData().addCommand(Command.ROTATE_CW);
+                break;
+
+        }
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
